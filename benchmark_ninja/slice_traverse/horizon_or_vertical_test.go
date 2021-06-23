e47d112295e1f0ddd7dd011003b8783770228d0e
Xargin <buaa.cch@gmail.com>
2021-06-23 15:37:46 +0800

    add horizontal && vertical traversal

---
+++ benchmark_ninja/slice_traverse/horizon_or_vertical_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+var arr = make([][]int,10000)
+
+func init(){
+	for i:=0;i<10000;i++ {
+		arr[i] = make([]int, 10000)
+	}
+}
+
+func BenchmarkHorizontal(b *testing.B) {
+	for i:=0;i< b.N;i++{
+		for x := 0;x < len(arr);x++{
+			for y := 0;y < len(arr);y++{
+				arr[x][y] = 1
+			}
+		}
+	}
+}
+
+func BenchmarkVertical(b *testing.B) {
+	for i:=0;i< b.N;i++{
+		for x := 0;x < len(arr);x++{
+			for y := 0;y < len(arr);y++{
+				arr[y][x] = 1
+			}
+		}
+	}
+}
