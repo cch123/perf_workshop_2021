96b2f5551d6ed42ff0983ed0a1f9ac8b61cf0123
Xargin <buaa.cch@gmail.com>
2021-06-23 17:09:26 +0800

    add compiler 101

--- benchmark_ninja/slice_traverse/horizon_or_vertical_test.go
+++ benchmark_ninja/slice_traverse/horizon_or_vertical_test.go
@@ -2,18 +2,18 @@ package main
 
 import "testing"
 
-var arr = make([][]int,10000)
+var arr = make([][]int, 10000)
 
-func init(){
-	for i:=0;i<10000;i++ {
+func init() {
+	for i := 0; i < 10000; i++ {
 		arr[i] = make([]int, 10000)
 	}
 }
 
 func BenchmarkHorizontal(b *testing.B) {
-	for i:=0;i< b.N;i++{
-		for x := 0;x < len(arr);x++{
-			for y := 0;y < len(arr);y++{
+	for i := 0; i < b.N; i++ {
+		for x := 0; x < len(arr); x++ {
+			for y := 0; y < len(arr); y++ {
 				arr[x][y] = 1
 			}
 		}
@@ -21,9 +21,9 @@ func BenchmarkHorizontal(b *testing.B) {
 }
 
 func BenchmarkVertical(b *testing.B) {
-	for i:=0;i< b.N;i++{
-		for x := 0;x < len(arr);x++{
-			for y := 0;y < len(arr);y++{
+	for i := 0; i < b.N; i++ {
+		for x := 0; x < len(arr); x++ {
+			for y := 0; y < len(arr); y++ {
 				arr[y][x] = 1
 			}
 		}
